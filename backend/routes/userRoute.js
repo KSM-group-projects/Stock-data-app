accf29be6b338809d9ff30594cf948e9c6b1058c
Dinesh choudhary <dinesh.choudhary.rrc@gmail.com>
2022-11-06 08:59:16 +0530

    done login,logout logic with validation

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 //controllers pull
-const {registerUser} = require("../controllers/userController");
+const {registerUser, loginUser,logout} = require("../controllers/userController");
 
 router.post("/register",registerUser)
-
+router.post("/login",loginUser);
+router.get("/logout",logout);
 
 module.exports = router
