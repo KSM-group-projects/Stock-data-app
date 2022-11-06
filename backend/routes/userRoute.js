4d9e22601d26e1f2d3a15b248d10ecfbdf2e5817
Dinesh choudhary <dinesh.choudhary.rrc@gmail.com>
2022-11-06 18:03:44 +0530

    emailsending,forgot and reset password

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 //controllers pull
-const {registerUser, loginUser,logout, getUser, loginStatus} = require("../controllers/userController");
+const {registerUser, loginUser,logout, getUser, loginStatus, updateUser, changePassword, forgotPassword, resetPassword} = require("../controllers/userController");
 const protect = require("../middleware/authMiddleware");
 
 router.post("/register",registerUser);
@@ -9,4 +9,9 @@ router.post("/login",loginUser);
 router.get("/logout",logout);
 router.get("/getuser",protect, getUser);
 router.get("/loggedin",loginStatus);
+router.patch("/updateuser",protect, updateUser);
+router.patch("/changepassword", protect,changePassword);
+router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword/:resetToken",resetPassword);
+
 module.exports = router;
